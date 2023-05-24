e470104ace96f32a6cd467e7dc3f32f397c7060c
Ryan-Duffett <124389615+Ryan-Duffett@users.noreply.github.com>
2023-05-24 16:55:22 +0000

    did work

--- js/script.js
+++ js/script.js
@@ -3,3 +3,24 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Ryan Duffet
+// Created on: Sep 2020
+// This file contains the JS functions for index.html
+
+'use strict'
+
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function calculate () {
+  // input
+  const fahrenheit = parseFloat(document.getElementById('fahrenheit').value)
+
+  // process
+  const celcius = (fahrenheit - 32) * 5/9
+
+  // output
+  document.getElementById('celcius').innerHTML = 'The temperature is: ' + celcius.toFixed(2) + ' °C '
+}
